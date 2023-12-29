92a5fc1a3f992017987afa4b068c203e09e7bad5
Clifford William <ccliffordwilliam@gmail.com>
2023-12-29 15:51:40 +0800

    feat(typing anim): typing anim

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Home() {
   return (
     <main>
-      <h2>Home</h2>
+      <h2 className="hero-title" aria-label="Hi! I'm a developer">
+        Hi! I'm a&nbsp;<span class="typewriter"></span>
+      </h2>
     </main>
   );
 }
